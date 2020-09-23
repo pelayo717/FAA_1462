1b319ef07fa140d4556f667621962f58a2a20513
juan.hermoso <you@example.com>
2020-09-23 21:11:54 +0200

    Cambiados argumentos de entrada al programa

--- P0/MainEntrada.py
+++ P0/MainEntrada.py
@@ -1,5 +1,11 @@
 from Datos import Datos
+import sys
 
 if __name__ == "__main__":
 
-    datos = Datos("ConjuntosDatos/german.data")
+    if ( len(sys.argv) != 2):
+        print("Numero de argumentos de entrada incorrectos.")
+
+    else:
+        fileName = "ConjuntosDatos/"  + sys.argv[1]
+        datos = Datos(fileName)
