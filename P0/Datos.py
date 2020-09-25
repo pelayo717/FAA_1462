6b35df6013e0ee457747c7f050f48e8e2a3703c8
pelayo717 <pelayorodriguez4@gmail.com>
2020-09-25 17:48:02 +0200

    falta comentar/revisar

--- P0/Datos.py
+++ P0/Datos.py
@@ -18,15 +18,15 @@ class Datos:
 
         #=============================== ATRIBUTOS ======================================= #
 
-        atributos = list(datosEntrada.columns)
+        self.atributos = list(datosEntrada.columns)
 
-        cantidadAtributos = len(atributos) # Variable auxiliar que almacena el numero de atributos del dataset
+        cantidadAtributos = len(self.atributos) # Variable auxiliar que almacena el numero de atributos del dataset
         cantidadDatos = len(datosEntrada)  # Variable auxiliar que almacena el numero de datos por columna del dataset
         
         #============================= NOMINALATRIBUTOS ===================================== #
 
         # ARRAY de dimension (columnas) de tipo booleano
-        nominalAtributos = np.empty(cantidadAtributos, dtype=bool)
+        self.nominalAtributos = np.empty(cantidadAtributos, dtype=bool)
         
         # Descartamos la ultima columna, que solo nos indica el valor de la clase
         for i in range(cantidadAtributos):
@@ -35,22 +35,22 @@ class Datos:
             # casteamos si es posible de string ==> float (incluimos reales y enteros)
             try:
                 float(aux)
-                nominalAtributos[i] = False
+                self.nominalAtributos[i] = False
             except ValueError:
-                nominalAtributos[i] = True    
+                self.nominalAtributos[i] = True    
 
 
         # =========================== DICCIONARIO ============================ #
 
-        diccionario={}
+        self.diccionario={}
 
         # Revisamos si son valores nominales o enteros/reales
         for h in range(cantidadAtributos):
-            if(nominalAtributos[h] == True):
+            if(self.nominalAtributos[h] == True):
                 # Creamos la lista auxiliar, en la que aniadiremos los valores de una columna, sin repeticion
                 lista_aux =[]
 
-                for i in datosEntrada[atributos[h]]:
+                for i in datosEntrada[self.atributos[h]]:
                     if i not in lista_aux:
                         lista_aux.append(i)
           
@@ -62,30 +62,30 @@ class Datos:
                 for i in lista_aux:
                     diccionario_aux[i] = lista_aux.index(i)
 
-                diccionario[atributos[h]] = diccionario_aux
+                self.diccionario[self.atributos[h]] = diccionario_aux
 
                 #Liberamos los contenidos de la lista y el diccionario auxiliar para la siguiente columna (atributo)
                 diccionario_aux=None
                 lista_aux=None
             else:
-                diccionario[atributos[h]] = {}
+                self.diccionario[self.atributos[h]] = {}
 
 
         # ============================= DATOS =============================== #
 
         # ARRAY bidimensional de tipo numerico, usamos numero de filas y de columnas de la variable datosEntrada
-        datos = np.empty([cantidadDatos, 0],dtype=object)
+        self.datos = np.empty([cantidadDatos, 0],dtype=object)
         
         for i in range(cantidadAtributos): # Para cada uno de los atributos
-            nombreAtributo = atributos[i]
+            nombreAtributo = self.atributos[i]
             columnaAtributos = []
 
             # Comprobamos si el tipo de dato es nominal
-            if ( nominalAtributos[i] == True ):
+            if ( self.nominalAtributos[i] == True ):
                 for j in datosEntrada[nombreAtributo]: # Para cada uno de los valores
 
                     # Buscamos en el diccionario sus pares clave-valor
-                    valor = diccionario[nombreAtributo]
+                    valor = self.diccionario[nombreAtributo]
 
                     # Encontramos la conversion del atributo nominal a numerico
                     entrada = valor[j]
@@ -98,9 +98,8 @@ class Datos:
                     columnaAtributos.append(j)
             
             columnaAtributos = np.array([columnaAtributos]).transpose()
-            datos = np.append(datos, columnaAtributos, axis=1)
+            self.datos = np.append(self.datos, columnaAtributos, axis=1)
 
-        
     # TODO: implementar en la practica 1
     def extraeDatos(self, idx):
         pass
