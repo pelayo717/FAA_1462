f6a3d93a45b5303ba628360f099c811b4f59b64f
juan.hermoso <you@example.com>
2020-09-28 20:15:13 +0200

    Terminada validacion simple a falta de pruebas

--- P1/Datos.py
+++ P1/Datos.py
@@ -1,13 +1,110 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
 
-# coding: utf-8
 import pandas as pd
-#import numpy as np
+import numpy as np
+import string
 
 class Datos:
 
-    # TODO: procesar el fichero para asignar correctamente las variables nominalAtributos, datos y diccionarios
-    def __init__(self, nombreFichero):       
-        
-    def extraeDatos(self,idx):
+    nominalAtributos = None     # Array de tipo booleano que indica si el tipo de atributo es nominal
+    atributos = None            # Array de tipo string con los nombres de los atributos
+    datos = None                # Array bidimensional 'numpy' con los datos de los atributos
+    diccionario = None          # Diccionario con los pares clave-valor de las conversiones de atributos nominales
+    cantidadDatos = None        # Entero que contiene el numero de filas del dataset
 
+    # TODO: procesar el fichero para asignar correctamente las variables nominalAtributos, datos y diccionario
+    def __init__(self, nombreFichero):
+
+        # Leemos del fichero csv los datos 
+        datosEntrada = pd.read_csv(nombreFichero)
+
+        #=============================== ATRIBUTOS ======================================= #
+
+        # Creamos una lista con los nombres de los atributos 
+        self.atributos = list(datosEntrada.columns)
+
+        cantidadAtributos = len(self.atributos) # Variable auxiliar que almacena el numero de atributos del dataset
+        self.cantidadDatos = len(datosEntrada)  # Variable auxiliar que almacena el numero de datos por columna del dataset
+        
+        #============================= NOMINALATRIBUTOS ===================================== #
+
+        # Array de una dimension (columnas) de tipo booleano
+        self.nominalAtributos = np.empty(cantidadAtributos, dtype=bool)
+        
+        # Verficamos todas las columnas, incluida la clasificacion de los datos
+        for i in range(cantidadAtributos):
+            aux = datosEntrada.values[0][i]
+
+            # casteamos si es posible de string ==> float (incluimos reales y enteros)
+            try:
+                float(aux)
+                self.nominalAtributos[i] = False
+            except ValueError:
+                self.nominalAtributos[i] = True    
+
+
+        # =========================== DICCIONARIO ============================ #
+
+        self.diccionario={}
+
+        # Revisamos si son valores nominales o enteros/reales
+        for h in range(cantidadAtributos):
+            if(self.nominalAtributos[h] == True):
+                # Creamos la lista auxiliar, en la que aniadiremos los valores de una columna, sin repeticion
+                lista_aux =[]
+
+                for i in datosEntrada[self.atributos[h]]:
+                    if i not in lista_aux:
+                        lista_aux.append(i)
+          
+                # Lista con los elementos albergados y listo para ordenar
+                lista_aux.sort()
+
+                # Creamos diccionario auxiliar, donde insertar las claves correctamente junto a los valores correspondientes
+                diccionario_aux = {}
+                for i in lista_aux:
+                    diccionario_aux[i] = lista_aux.index(i)
+
+                # Asignamos el diccionario auxiliar a la entrada del diccionario general
+                self.diccionario[self.atributos[h]] = diccionario_aux
+
+                # Liberamos los contenidos de la lista y el diccionario auxiliar para la siguiente columna (atributo)
+                diccionario_aux=None
+                lista_aux=None
+            else:
+                # De ser valores enteros/reales creamos un diccionario vacio
+                self.diccionario[self.atributos[h]] = {}
+
+
+        # ============================= DATOS =============================== #
+
+        # Array bidimensional de tipo numerico, usamos numero de filas y de columnas de la variable datosEntrada
+        self.datos = np.empty([self.cantidadDatos, 0],dtype=object)
+        
+        for i in range(cantidadAtributos): # Para cada uno de los atributos
+            nombreAtributo = self.atributos[i]
+            columnaAtributos = []
+
+            # Comprobamos si el tipo de dato es nominal
+            if ( self.nominalAtributos[i] == True ):
+                for j in datosEntrada[nombreAtributo]: # Para cada uno de los valores
+
+                    # Buscamos en el diccionario sus pares clave-valor
+                    valor = self.diccionario[nombreAtributo]
+
+                    # Encontramos la conversion del atributo nominal a numerico
+                    entrada = valor[j]
+                    columnaAtributos.append(entrada)
+
+            # Si el dato ya es de tipo numerico, se introduce
+            else:
+                for j in datosEntrada[nombreAtributo]: # Para cada uno de los valores
+                    
+                    columnaAtributos.append(j)
+            
+            columnaAtributos = np.array([columnaAtributos]).transpose()
+            self.datos = np.append(self.datos, columnaAtributos, axis=1)
+
+    # TODO: implementar en la practica 1
+    def extraeDatos(self, idx):
+        pass
--- P1/EstrategiaParticionado.py
+++ P1/EstrategiaParticionado.py
@@ -3,7 +3,7 @@ from abc import ABCMeta,abstractmethod
 
 class Particion():
 
-  # Esta clase mantiene la lista de índices de Train y Test para cada partición del conjunto de particiones  
+  # Esta clase mantiene la lista de indices de Train y Test para cada particion del conjunto de particiones  
   def __init__(self):
     self.indicesTrain=[]
     self.indicesTest=[]
@@ -17,6 +17,8 @@ class EstrategiaParticionado:
   
   # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor 
   
+  error = None        # Porcentaje de error del modelo
+
   @abstractmethod
   # TODO: esta funcion deben ser implementadas en cada estrategia concreta  
   def creaParticiones(self,datos,seed=None):
@@ -27,13 +29,53 @@ class EstrategiaParticionado:
 
 class ValidacionSimple(EstrategiaParticionado):
   
-  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el número de ejecuciones deseado
+  porcentaje = None       # Porcentaje de la muestra que se utilizara como entrenamiento
+  numEjecuciones = None   # Numero de ejecuciones de la validaciÃ³n simple
+
+  def __init__(self, por, num):
+    self.porcentaje = por
+    self.numEjecuciones = num
+
+  
+  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el nï¿½mero de ejecuciones deseado
   # Devuelve una lista de particiones (clase Particion)
   # TODO: implementar
-  def creaParticiones(self,datos,seed=None):
+  def creaParticiones(self, datos, seed=None):
+    # Control de errores por si el parametro seed es 'None'
+    if seed is None:
+      seed = 10
+
+    #Generamos la semilla
+    random.seed(seed) 
+    listaParticiones = [] # Lista de tipo Particion con las particiones generadas
+
+    # Cantidad de datos de la muestra que se cogeran como datos de testeo
+    numTest = datos.cantidadDatos * (1 - ( self.porcentaje / 100 ))
     
-    random.seed(seed)
-    pass
+    # Creamos una particion nueva por cada ejecucion y la aniadimos a la lista
+    for i = 0 in range( self.numEjecuciones ):
+      particion = Particion()
+      
+      # Metemos todos los indices posibles en train. Generamos indices aleatorios y los metemos en test sacandolos de train.
+      # De esta forma podemos comprobar si ya se han repetido y los restantes de train ya se encuentran en test.
+      particion.indicesTrain = list(range(0, datos.cantidadDatos))
+
+      # Insertamos todos los indices generados aleatoriamente en Test y los eliminamos de Train
+      for j = 0 in range( numTest ):
+        
+        encontrado = False        # Flag que garantiza que el indice generado no estÃ© repetido
+        while encontrado is False:
+
+          indice = random.randint(0, (datos.cantidadDatos - 1))
+          # Comprobamos si se encuentra todavia en Train
+          if indice in particion.indicesTrain:
+            particion.indicesTest.append(indice)
+            particion.indicesTrain.remove(indice)
+            encontrado = True
+      
+      listaParticiones.append(particion)
+
+    return listaParticiones
       
       
 #####################################################################################################      
