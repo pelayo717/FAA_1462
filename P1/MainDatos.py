30ddfaa0d9927016e3308833e95de0adcadea20d
juan.hermoso <you@example.com>
2020-10-11 12:11:23 +0200

    Cambios en el main

--- P1/MainDatos.py
+++ P1/MainDatos.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     
     # Probamos con 20 porciento y 3 iteraciones (Validacion Simple)
     validacion_simple = ValidacionSimple(20,3)
-    #aux_simple = validacion_simple.creaParticiones(datos)
+    aux_simple = validacion_simple.creaParticiones(datos)
 
     # Probamos con 2 k-iteraciones
-    #validacion_cruzada = ValidacionCruzada(3)
-    #aux_cruzada = validacion_cruzada.creaParticiones(datos)
+    validacion_cruzada = ValidacionCruzada(3)
+    aux_cruzada = validacion_cruzada.creaParticiones(datos)
 
     """print(aux_cruzada)
     print(len(aux_cruzada[0].indicesTrain[0]))
